e41e9fba11c22df495a2cd74c69f8ec72470f1b6
santiUninorte <eddiem@uninorte.edu.co>
2022-09-08 20:35:42 -0500

    se agrego pez

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -3,7 +3,8 @@ public class GeneradorVertebrados {
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
+        miVertebrados[2] = new Vertebrado("Tiburón", "Pez");
+        miVertebrados[3] = new Vertebrado("orca", "Pez"); 
         
         return miVertebrados;
     }
